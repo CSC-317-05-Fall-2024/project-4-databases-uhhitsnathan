82544d9c14dd67941920b0af10f02bf500a9cf86
uhhitsnathan <nathans2003@icloud.com>
2024-11-03 22:32:05 -0800

    added all code to support railway and seed db script

--- server/config/database.js
+++ server/config/database.js
@@ -1,5 +1,11 @@
 /* Establish the DB connection pool here. */
 import pg from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
+
 const config = {
+    connectionString: process.env.CONNECTION_STRING
 };
-export const pool = new pg.Pool(config);
+
+const pool = new pg.Pool(config);
+export {pool};
--- server/config/seed-db.js
+++ server/config/seed-db.js
@@ -3,6 +3,7 @@ import { pool } from './database.js';
 
 const dropTables = async () => {
     try {
+        console.log('dropping tables...');
         const dropTablesQuery = `
             DROP TABLE IF EXISTS restaurants;
         `;
@@ -14,6 +15,18 @@ const dropTables = async () => {
 
 const createTables = async () => {
     try {
+        console.log('creating restaurants...');
+        const createQuery = `
+            CREATE TABLE IF NOT EXISTS restaurants (
+                id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+                name TEXT NOT NULL,
+                phone TEXT NOT NULL,
+                address TEXT NOT NULL,
+                photo TEXT
+            );
+        `;
+        await pool.query(createQuery);
+        console.log('created restaurants');
     } catch (error) {
         console.log(error)
     }
@@ -21,8 +34,24 @@ const createTables = async () => {
 
 const insertData = async () => {
     try {
+        console.log('adding initial data...');
+        const insertQuery = `
+    INSERT INTO restaurants (name, phone, address, photo)
+    VALUES 
+        ('Mom Can Cook Thai Kitchen', '(661) 251-8103', '18358 Soledad Canyon Rd, Santa Clarita, CA 91387', '/images/download.jpeg'),
+        ('Toppers Pizza', '(661) 222-7888', '8417 Soledad Canyon Rd, Santa Clarita, CA 91387', '/images/toppers.jpeg'),
+        ('Gyromania', '(661) 252-4976', '20655 Soledad Canyon Rd, Santa Clarita, CA 91351', '/images/gyro.jpeg'),
+        ('The Stand', '(661) 339-2333', '24201 Valencia Blvd Suite 3260, Valencia, CA 91355', '/images/stand.jpeg'),
+        ('Charcoal Korean BBQ', '(661) 251-9292', '19158 Soledad Canyon Rd, Canyon Country, CA 91351', '/images/charcoal2.jpg'),
+        ('Marston''s Restaurant', '(661) 253-9910', '24011 Newhall Ranch Rd, Valencia, CA 91355', '/images/martsons3.jpeg'),
+        ('Chi-Chis Pizza', '(661) 252-4405', '27117 Sierra Hwy, Canyon Country, CA 91351', '/images/chi.jpeg'),
+        ('IN-N-Out Burger', '(800) 786-1000', '28368 Sand Canyon Rd, Santa Clarita, CA 91351', '/images/out3.jpeg'),
+        ('Bonsai Sushi Garden', '(661) 251-9008', '19358 Soledad Canyon Rd, Santa Clarita, CA 91351', '/images/bonsai.jpg');
+`;
+
+        await pool.query(insertQuery);
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 }
 
